e96b6645028547c0a8d8da8eb108b5d72c8cb30e
hassan-thabet <dev.7assan@gmail.com>
2023-05-05 12:10:54 +0300

    update sll cert

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        URL::forceRootUrl(Config::get('app.url'));
+        if (str_contains(Config::get('app.url'), 'https://')) {
+            URL::forceScheme('https');
+        }
     }
 }
