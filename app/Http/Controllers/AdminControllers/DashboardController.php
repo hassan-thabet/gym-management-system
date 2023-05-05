ecebba5cd7eff9c5da2bf66f4324217960053166
hassan-thabet <dev.7assan@gmail.com>
2023-05-05 11:53:32 +0300

    update dashboard

--- app/Http/Controllers/AdminControllers/DashboardController.php
+++ app/Http/Controllers/AdminControllers/DashboardController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard.index');
+        return view('dashboard.home.index');
     }
 
 }
